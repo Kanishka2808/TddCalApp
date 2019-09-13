8e14d6fa18adaf49f965b025893c7dd93cffbcf8
Kanishka2808 <wadhwakanishka01@gmail.com>
2019-09-13 10:29:58 +0530

    Second Commit for fail!!!

---
+++ TddCalApp.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TddCalApp.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TddCalApp.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a80310a-3495-48bd-8082-978f29920c49")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TddCalApp.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddCalApp.library;
+
+namespace TddCalApp.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldReturnPositiveonPassingTwoPositiveNumbers()
+        {
+            Calculator testCalculator = new Calculator();
+            int num1 = 3;
+            int num2 = 2;
+            int result;
+            result = testCalculator.Add(num1, num2);
+            Assert.AreEqual(5, "Testing two integers 3 and 2");
+        }
+    }
+}
